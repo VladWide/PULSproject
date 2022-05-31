933039b44ea89c6c3a0d2472c51207b21514dca0
Vlad <shyrokij009@gmail.com>
2022-05-31 14:54:36 +0300

    section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,21 @@ $(document).ready(function(){
         ]
     }
     );
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_activ)', function() {
+        $(this)
+          .addClass('catalog__tab_activ').siblings().removeClass('catalog__tab_activ')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+      });
+
+     function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_activ');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_activ');
+            })
+        });
+     };
+     toggleSlide('.catalog-item__link');
+     toggleSlide('.catalog-item__back');
   });
